63d22957c3f7c216ba2e5b742d96f49a58c8d5d9
HCH <1946824970@qq.com>
2021-12-04 20:31:47 +0800

    change

---
+++ src/main/java/com/ooad/oj_backend/mybatis/entity/UserResult.java
@@ -0,0 +1,15 @@
+package com.ooad.oj_backend.mybatis.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+@Getter
+@Setter
+public class UserResult implements Serializable {
+    private String userName;
+    private String shownId;
+    private long time;
+    private int score;
+    private int totalScore;
+}
